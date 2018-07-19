984512ba339b882f679f38eba381c72f62fb2aad
Tucker-Adamson <Code@batc.local>
2018-07-19 09:09:55 -0600

    foahgaf

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import { reduxForm, Field} from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 import { FormTitle } from '../formTitle';
-import { FormInput } from '../formFields';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from "../textLink";
 
 class SigninForm extends Component {
     render() {
         return (
             <form>
-                <FormTitle classname='sign-in__title' text='Login'/>
-                <Field className='sign-in-form__email' component={FormInput} name='email' type='email'/>
+                <FormTitle classname='sign-in__title' text='Login' />
+                <Field 
+                className='sign-in-form__email' 
+                placehoder='Enter Email' 
+                component={FormInput} 
+                name='email' 
+                type='email'
+                title="Email"
+                component={FormInput}
+                />
+                <Field 
+                className='sign-in-form__password' 
+                placehoder='Enter Password' 
+                component={FormInput} 
+                name='password' 
+                type='password'
+                title="Password"
+                component={FormInput}
+                />
+                <Field 
+                className='sign-in-form__login' 
+                component={FormInput} 
+                name='login' 
+                type='submit'
+                title="login"
+                component={FormButton}
+                />
+                <div className='sign-in-form__text-links'>
+                    <TextLink to='/forgot' text='Forgot Password'/>
+                    <TextLink to='/signup' text='Sign Up'/>
+                </div>
             </form>
         )
     }
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
     export class FormInput extends Component {
         render() {
-            const { className, title } = this.props
+            const { className, title, input, type } = this.props
             return(
                 <div classname='form-input'>
-                    <label>{title}</label>
-                    <input className={`${className} form-input`}/>
+                    <label classname='form-input__title'>{title}</label>
+                    <input 
+                        className={`${className} form-input__input`}
+                        type={type}
+                        {...input}
+                        placeholder={placeholder}
+                        />
                 </div>
             )}
         }
+    
+        export class FormButton extends Component {
+            render() {
+                const { className, title, input, type } = this.props
+                return(
+                    <div classname='form-button'>
+                        <button
+                            className={`${className} form-button__button`}
+                            type={type}
+                            {...input}
+                            >
+                            {title}
+                            </button>
+                    </div>
+                )}
+            }
